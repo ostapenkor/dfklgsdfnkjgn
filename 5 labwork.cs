4969047909fb3f6af4e4d6f19f8740fa7c78ae2b
Roman Ostapenko <romanostapenko062004@gmail.com>
2021-11-30 07:56:52 +0800

    commit 1

---
+++ 5 labwork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace sdddsfs
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            //float p, t,n;
+            //for(float x = 0; x <= 4; x++)
+            //{
+            //    if (x % 2 == 0)
+            //    {
+            //        t = 2;
+            //    }
+            //    else
+            //    {
+            //        t = 1.5f;
+            //    }
+            //    n = 1;
+            //    p = 1;
+            //    while (n <= t * x)
+            //    {
+            //        p *= n;
+            //        Console.WriteLine($"\tn = {n}   p = {p}");
+            //        n++;
+            //    }
+            //    Console.WriteLine($"x = {x}   p = {p}");
+            //}
+            
+            
+                Console.WriteLine(Computer.GetDistance(55.7539, 37.6208, 59.9398, 30.3146));
+                Console.ReadKey();
+            
+
+            Console.ReadKey();
+        }
+    }
+}
